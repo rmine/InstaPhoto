48a574d9ae1fd1be931c80f092f8851c9f2f445b
huangzhaorong <huangzhaorong@huangzhaorongdeMac-mini.local>
2015-03-20 11:20:09 +0800

    Initial Commit

---
+++ InstaPhoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaPhoto
+//
+//  Created by huangzhaorong on 15/3/20.
+//  Copyright (c) 2015年 huangzhaorong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ InstaPhoto/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  InstaPhoto
+//
+//  Created by huangzhaorong on 15/3/20.
+//  Copyright (c) 2015年 huangzhaorong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
